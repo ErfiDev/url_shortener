417aedba8650e3636a42306a0705f30890517a57
erfan <erfanhanifezade@gmail.com>
2022-07-20 20:54:28 +0430

    add views to fiber

--- delivery/setup.go
+++ delivery/setup.go
@@ -4,11 +4,19 @@ import (
 	"github.com/erfidev/url_shortener/contract"
 	"github.com/erfidev/url_shortener/env"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
 	"log"
 )
 
 func Setup(db contract.DbContract, app *env.AppEnv) {
-	r := fiber.New()
+	viewEngine := html.New("./views", ".gohtml")
+
+	r := fiber.New(fiber.Config{
+		Views: viewEngine,
+	})
+
+	// home
+	r.Get("/", )
 
 	log.Printf("router initialized, listening port: %s\n", app.Port)
 	err := r.Listen(app.Port)
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gofiber/fiber/v2 v2.35.0 // indirect
+	github.com/gofiber/template v1.6.29 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
